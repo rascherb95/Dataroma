5614ecca71a8f2f0e6a589acba88dc5f6f59d2bc
rascherb95 <67018023+rascherb95@users.noreply.github.com>
2021-09-20 00:17:20 -0400

    403 error workaround
    added user:agent to scrape request to work around 403 error

--- dataromascraper.py
+++ dataromascraper.py
@@ -1,54 +1,65 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 import pandas as pd
 import matplotlib.pyplot as plt
 
 base_url = 'https://www.dataroma.com'
+yahoo_base = 'https://finance.yahoo.com/quote/'
 
 
 #to create a list of our investors
 def get_investors():
 
-    investors = []
-    html = urlopen(base_url)
+    investors_scrape = []
+    req = Request(base_url , headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
+    html = urlopen(req,data=None)
     soup = BeautifulSoup(html.read(),'lxml')
 
     for link in soup.find('span',{'id':'port_body'}).find_all('a'):
-        investors.append((Investor(link.getText(),link.get('href'))))
-    return (investors)
+        investors_scrape.append((Investor(link.getText(),link.get('href'))))
+    return (investors_scrape)
 
 #investor class
 class Investor():
    
     def __init__(self,name,investor_url):
-        self.name= name
+        self.name = name
         self.url = base_url + investor_url
 
     def get_portfolio(self):
-        html = urlopen(self.url)
+        Investor_req = Request(self.url,headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
+        html = urlopen(Investor_req) # request needs to have user:agent to avoid 403 error
+
         soup = BeautifulSoup(html.read(),'lxml')
 
         portfolio = []
-        tbody= soup.tbody
+        tbody = soup.tbody
         tr = tbody.find_all('tr')
 
         for t in tr:
             portfolio.append(t.text.split('\n'))
             df = pd.DataFrame(portfolio)
+        
 
 #reformatting the dataframe
-        df.columns = [0,1,'Ticker','Weight','Shares','Action','Price','Value','']
-        df['Ticker'] = df['Ticker'].str.split('-', expand=True)  #to update split to make 2 columns, ticker + company
+        df.columns = [0,1,'Ticker','Weight','Shares','Action','Price','Rounded Value','True value']
+        df['Ticker'] = df['Ticker'].str.split('-', expand=True)  #self note to update split to make 2 columns, ticker + company
         df['Weight'] = df['Weight'].astype(float)/100
+        df['Shares'] = df['Shares'].str.replace(',','').astype(int)
+        df['Price']  = df['Price'].str.replace('$','').astype(float)
+        df['True value'] = df['Shares'] * df['Price']
         #df['Company'] = df['Stock'].str.rsplit('-')
         df = df.drop(columns = [0,1])
+
+        df.to_excel('output.xlsx',index=False)
+
         return df
-        
-        
+
+             
 #to get our chosen investor's portfolio
 def get_specific(arg):
-    for i in x:
-        if arg in i.name:
+    for i in investor_list: #for each investor in our investor list (investor_scrape dict)
+        if arg in i.name: #if name is in investor.name,
             print("-"*100)
             print(i.name)
             print("-"*100)
@@ -57,21 +68,25 @@ def get_specific(arg):
     else:
         print("-"*100)
         print("{} not found".format(arg)) 
+    
 
-#should be renamed investor_list for easier readability
-x = get_investors()
+investor_list = get_investors()
 
 
-# to print out the list of available filings. could add prompt (See available portfolios? Y/N)
-#for i in x:
-#    print(i.name)
+#to print out the list of available filings
+for i in investor_list:
+    print(i.name)
 
 print('Fund manager last name:')
 chosen_investor = (input())
 chosen_investor = chosen_investor.capitalize()
 
-
 #runs everything
 get_specific(chosen_investor)
+#export_portfolio(chosen_investor)
+
+
+
+
 
 
